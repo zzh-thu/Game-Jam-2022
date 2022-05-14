4786bee8850eb4fd08fafbbd57e9930c5a5898df
SophieShen <sophie.s.00510@gmail.com>
2022-05-14 13:52:30 +0800

    change the connectiong of animator
    now you can play animation by using simple bool

---
+++ GJ2022/Assets/Scripts/RobotController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Robot initial parameter")]
+    public float Velocity4RawMaterial;
+    public float Velocity4ProductCreate;
+    public float Velocity4RobotCreate;
+
+    [Space(50)]
+    public float Price4Sell;
+
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
